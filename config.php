4f6223655adbcabc03b10e47b7784403b6d8758b
yanndeo <yan2sambou@gmail.com>
2023-09-01 16:31:09 +0100

    initialize projec config prooph

--- config.php
+++ config.php
@@ -1,13 +1,31 @@
 <?php 
 
 namespace {
+use Prooph\Common\Event\ProophActionEventEmitter;
 use Prooph\Common\Messaging\FQCNMessageFactory;
+use Prooph\EventStore\ActionEventEmitterEventStore;
 use Prooph\EventStore\Pdo\MySqlEventStore;
 use Prooph\EventStore\Pdo\PersistenceStrategy\MySqlAggregateStreamStrategy;
+use Prooph\EventStoreBusBridge\EventPublisher;
+use Prooph\ServiceBus\EventBus;
 
 
 include "./vendor/autoload.php";
 
     $pdo = new PDO('mysql:dbname=prooph;host=localhost', 'root', '');
-    $eventStore = new MySqlEventStore(new FQCNMessageFactory(), $pdo, new MySqlAggregateStreamStrategy());
+    $eventStore = new MySqlEventStore(
+        new FQCNMessageFactory(),
+        $pdo,
+        new MySqlAggregateStreamStrategy());
+    
+    $eventEmitter = new ProophActionEventEmitter();
+    $eventStore = new ActionEventEmitterEventStore($eventStore, $eventEmitter);
+
+    $eventBus = new EventBus($eventEmitter);
+    $eventPublisher = new EventPublisher($eventBus);
+    $eventPublisher->attachToEventStore($eventStore);
+
+    $pdoSnapshotStore = new $pdoSnapshotStore($pdo);
+    $userRepository = new $userRepository($eve)
+
 }
